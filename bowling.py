fc293778b1babed6935c0c58da6c651562804fb8
bryce <bryce.williams@thoughtworks.com>
2024-09-17 14:52:17 -0500

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:bowling.py

--- bowling.py
+++ bowling.py
@@ -1,2 +1,10 @@
 def score_cruncher(frames):
+    scores_per_frame = frames.split(" ")
+    frame_one_score = scores_per_frame[0]
+
+    roll1 = frame_one_score[0]
+    roll2 = frame_one_score[1]
+
+    parse_roll(roll1) + parse_roll(roll2)
+
     return 0
--- testing_test.py
+++ testing_test.py
@@ -1,6 +1,11 @@
 import pytest
 import bowling
 
-def test_pytest_is_working():
-    total_score = bowling.score_cruncher([0, 0, 0])
+def test_all_gutter_balls():
+    total_score = bowling.score_cruncher("-- -- -- -- -- -- -- -- -- --")
     assert total_score == 0
+
+
+def test_score_of_one_should_return_one():
+    total_score = bowling.score_cruncher("-1 -- -- -- -- -- -- -- -- --")
+    assert total_score == 1
