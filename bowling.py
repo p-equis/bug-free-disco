adfef49f8e689a5b44b961aad7d95f14f4e3352b
Lindsey Coffee-Johnson <lcoffeejohnson@gmail.com>
2024-09-17 16:00:22 -0400

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:bowling.py

--- bowling.py
+++ bowling.py
@@ -5,6 +5,11 @@ def score_cruncher(frames):
     roll1 = frame_one_score[0]
     roll2 = frame_one_score[1]
 
-    parse_roll(roll1) + parse_roll(roll2)
+    return parse_roll(roll1) + parse_roll(roll2)
 
-    return 0
+
+def parse_roll(roll):
+    if roll == "-":
+        return 0
+    else:
+        return int(roll)
--- testing_test.py
+++ testing_test.py
@@ -9,3 +9,7 @@ def test_all_gutter_balls():
 def test_score_of_one_should_return_one():
     total_score = bowling.score_cruncher("-1 -- -- -- -- -- -- -- -- --")
     assert total_score == 1
+
+def test_score_of_single_strike():
+    total_score = bowling.score_cruncher("x -- -- -- -- -- -- -- -- --")
+    assert total_score == 10
