9aad5fce731575f5fc139eae2c55708f0ef4f8db
Lindsey Coffee-Johnson <lcoffeejohnson@gmail.com>
2024-09-19 16:50:14 -0400

    green

--- bowling.py
+++ bowling.py
@@ -2,10 +2,13 @@ def score_cruncher(frames):
     scores_per_frame = frames.split(" ")
     frame_one_score = scores_per_frame[0]
 
-    roll1 = frame_one_score[0]
-    roll2 = frame_one_score[1]
-
-    return parse_roll(roll1) + parse_roll(roll2)
+    match(list(frame_one_score)):
+        case ['x']: 
+            return 10
+        case [a, '/']:
+            return 10
+        case [a, b]:
+            return parse_roll(a) + parse_roll(b)
 
 
 def parse_roll(roll):
--- testing_test.py
+++ testing_test.py
@@ -13,3 +13,7 @@ def test_score_of_one_should_return_one():
 def test_score_of_single_strike():
     total_score = bowling.score_cruncher("x -- -- -- -- -- -- -- -- --")
     assert total_score == 10
+
+def test_score_of_spare():
+    total_score = bowling.score_cruncher("5/ -- -- -- -- -- -- -- -- --")
+    assert total_score == 10
