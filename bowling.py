bc155436eee38f3ba06db122744680c6a6d0a0fa
Paul Phillips <pphillips@ritchiebros.com>
2024-09-20 14:17:48 -0500

    green

--- bowling.py
+++ bowling.py
@@ -5,25 +5,26 @@ def score_cruncher(frames):
 
 
     for frame_count in range(10):
-        frame = scores_per_frame[frame_count]
-        
-        if frame[0] == 'x':
-            next_frame = scores_per_frame[frame_count + 1]
-            next_next_frame = scores_per_frame[frame_count + 2]
-
-            total += strike_calculation(frame, next_frame, next_next_frame)
-        elif frame[1] == '/':
-
-            if frame_count == 9:
-                total += 10 + parse_roll(frame[2]) + parse_roll(frame[2])
-            else:
-                next_frame = scores_per_frame[frame_count + 1]
-                total += spare_calculation(frame, next_frame)
-        else:
-            total += parse_roll(frame[0]) + parse_roll(frame[1])
+        total += frame_score(frame_count, scores_per_frame)
 
     return total
 
+def frame_score(frame_count, scores_per_frame):
+    frame = scores_per_frame[frame_count]
+        
+    if frame[0] == 'x':
+        next_frame = scores_per_frame[frame_count + 1]
+        next_next_frame = scores_per_frame[frame_count + 2]
+
+        return strike_calculation(frame, next_frame, next_next_frame)
+    elif frame[1] == '/':
+        if frame_count == 9:
+            return 10 + parse_roll(frame[2]) + parse_roll(frame[2])
+        else:
+            next_frame = scores_per_frame[frame_count + 1]
+            return spare_calculation(frame, next_frame)
+    else:
+        return parse_roll(frame[0]) + parse_roll(frame[1])
 
 def parse_roll(roll):
     if roll == '-':
@@ -45,16 +46,3 @@ def strike_calculation(initial_frame, second_frame, third_frame):
         return 10 + parse_roll(second_frame[0]) + parse_roll(third_frame[0])
     else:
         return 10 + parse_roll(second_frame[0]) + parse_roll(second_frame[1]) 
-    
-def frame_score(frame, next_frame):
-    if list(frame)[0] == 'x':
-        return 10 + parse_roll(next_frame[0]) + parse_roll(next_frame[1])
-    
-    elif list(frame)[1] == '/':
-        if len(frame) == 3:
-            return 10 + (2 * parse_roll(frame[2]))
-        
-        return 10 + parse_roll(next_frame[0])
-    
-    else:
-        return parse_roll(frame[0]) + parse_roll(frame[1])
