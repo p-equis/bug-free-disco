e60011d1f4e61f02ecfe2e39923f8907204094da
Jack Hilscher <hilscherjack@johndeere.com>
2024-09-20 14:26:43 -0400

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:bowling.py

--- bowling.py
+++ bowling.py
@@ -6,11 +6,20 @@ def score_cruncher(frames):
     total = 0
 
 
-    for frame in range(10):
-        if frame >= 9:
-            total += frame_score(scores_per_frame[frame], None)
+    for frame_count in range(10):
+        frame = scores_per_frame[frame_count]
+
+        if frame[0] == 'x':
+            strike_calculation(frame, scores_per_frame[frame_count + 1], scores_per_frame[frame_count + 2], frame_count)
+        elif frame[1] == '/':
+
         else:
-            total += frame_score(scores_per_frame[frame], scores_per_frame[frame + 1])
+            return parse_roll(frame[0]) + parse_roll(frame[1])
+
+        # if frame >= 9:
+        #     total += frame_score(scores_per_frame[frame], None)
+        # else:
+        #     total += frame_score(scores_per_frame[frame], scores_per_frame[frame + 1])
 
     return total
 
@@ -20,11 +29,21 @@ def parse_roll(roll):
         return 0
     elif roll == 'x':
         return 10
+    elif roll == '/':
+        return 10
     else:
         return int(roll)
     
+def strike_calculation(intial_frame, second_frame, third_frame, frame_count):
+    if frame_count >= 9:
+        return parse_roll(intial_frame[0]) + parse_roll(intial_frame[1]) + parse_roll(intial_frame[2])
+    elif second_frame[0] == 'x':
+        return parse_roll(intial_frame[0]) + parse_roll(second_frame[0]) + parse_roll(third_frame[0])
+    else:
+        return parse_roll(intial_frame[0]) + parse_roll(second_frame[0]) + parse_roll(second_frame[1]) 
+    
 def frame_score(frame, next_frame):
-    if list(frame) == ['x']:
+    if list(frame)[0] == 'x':
         if next_frame[0] == 'x':
             return 10 + frame_score(next_frame, ["put something here, maybe recursion"])
         return 10 + parse_roll(next_frame[0]) + parse_roll(next_frame[1])
