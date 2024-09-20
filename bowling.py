9b7917f71c06e881454987e971141fba0594bebf
Paul Phillips <pphillips@ritchiebros.com>
2024-09-20 14:13:39 -0500

    green

--- bowling.py
+++ bowling.py
@@ -6,19 +6,21 @@ def score_cruncher(frames):
 
     for frame_count in range(10):
         frame = scores_per_frame[frame_count]
+        
+        if frame[0] == 'x':
+            next_frame = scores_per_frame[frame_count + 1]
+            next_next_frame = scores_per_frame[frame_count + 2]
 
-        # if frame[0] == 'x':
-        #     strike_calculation(frame, scores_per_frame[frame_count + 1], scores_per_frame[frame_count + 2])
-        # elif frame[1] == '/':
-        #     spare_calculation(frame, scores_per_frame[frame_count + 1])
+            total += strike_calculation(frame, next_frame, next_next_frame)
+        elif frame[1] == '/':
 
-        # else:
-        #     return parse_roll(frame[0]) + parse_roll(frame[1])
-
-        if frame_count >= 9:
-            total += frame_score(scores_per_frame[frame_count], None)
+            if frame_count == 9:
+                total += 10 + parse_roll(frame[2]) + parse_roll(frame[2])
+            else:
+                next_frame = scores_per_frame[frame_count + 1]
+                total += spare_calculation(frame, next_frame)
         else:
-            total += frame_score(scores_per_frame[frame_count], scores_per_frame[frame_count + 1])
+            total += parse_roll(frame[0]) + parse_roll(frame[1])
 
     return total
 
@@ -39,17 +41,13 @@ def spare_calculation(initial_frame, second_frame):
 
     
 def strike_calculation(initial_frame, second_frame, third_frame):
-    if len(initial_frame) == 3:
-        return parse_roll(initial_frame[0]) + parse_roll(initial_frame[1]) + parse_roll(initial_frame[2])
-    elif second_frame[0] == 'x':
-        return parse_roll(initial_frame[0]) + parse_roll(second_frame[0]) + parse_roll(third_frame[0])
+    if second_frame[0] == 'x':
+        return 10 + parse_roll(second_frame[0]) + parse_roll(third_frame[0])
     else:
-        return parse_roll(initial_frame[0]) + parse_roll(second_frame[0]) + parse_roll(second_frame[1]) 
+        return 10 + parse_roll(second_frame[0]) + parse_roll(second_frame[1]) 
     
 def frame_score(frame, next_frame):
     if list(frame)[0] == 'x':
-        if next_frame[0] == 'x':
-            return 10 + frame_score(next_frame, ["put something here, maybe recursion"])
         return 10 + parse_roll(next_frame[0]) + parse_roll(next_frame[1])
     
     elif list(frame)[1] == '/':
